b5223765bc7c97c9f0e0b84ab7b984846546185c
aminerve <122647915+aminerve@users.noreply.github.com>
2023-09-06 18:51:57 -0400

    excercise complete

--- src/main/java/SportStatistics.java
+++ src/main/java/SportStatistics.java
@@ -10,24 +10,46 @@ public class SportStatistics {
         Scanner scan = new Scanner(System.in);
         System.out.println("File:");
         String file = scan.nextLine();
+        System.out.println("Team:");
+        String team = scan.nextLine();
+        int gp = 0;
+        int wins = 0;
+        int losses = 0;
 
         try ( Scanner reader = new Scanner(Paths.get(file))) {
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
 
-                stats.add(line);
+                String[] parts = line.split(",");
+                String teamName = parts[0];
+                String oppName = parts[1];
+                int teamScore = Integer.valueOf(parts[2]);
+                int oppScore = Integer.valueOf(parts[3]);
+
+                if (teamName.equals(team)) {
+                    gp++;
+                    if (teamScore > oppScore) {
+                        wins++;
+                    }else{
+                        losses++;
+                    }
+                }
+                if (oppName.equals(team)){
+                    gp++;
+                    if (oppScore > teamScore){
+                        wins++;
+                    }else{
+                        losses++;
+                    }
+                }
+
             }
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
-        int gp = 0;
-        System.out.println("Team:");
-        String team = scan.nextLine();
-        for (String stat : stats) {
-            if (stat.contains(team)) {
-                gp++;
-            }
-        }
+
         System.out.println("Games: " + gp);
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
     }
 }

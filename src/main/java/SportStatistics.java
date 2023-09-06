1bc5b283c3c39e8832ae8446bd651e57e4e8570c
aminerve <122647915+aminerve@users.noreply.github.com>
2023-09-06 18:02:46 -0400

    query team stats functionality complete

--- src/main/java/SportStatistics.java
+++ src/main/java/SportStatistics.java
@@ -6,8 +6,26 @@ import java.util.Scanner;
 public class SportStatistics {
 
     public static void main(String[] args) {
+        ArrayList<String> stats = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
+        System.out.println("File:");
+        String file = scan.nextLine();
 
+        try ( Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+
+                stats.add(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        System.out.println("Team:");
+        String team = scan.nextLine();
+        for (String stat : stats) {
+            if (stat.contains(team)) {
+                System.out.println(stat);
+            }
+        }
     }
-
 }
